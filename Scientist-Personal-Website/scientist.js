75278d116d16743ad0afb5470c0d51da90e80c92
Slu5 <shuolu@hotmail.com>
2022-10-04 13:09:19 -0700

    Favorite Scientist

--- Scientist-Personal-Website/scientist.js
+++ Scientist-Personal-Website/scientist.js
@@ -4,3 +4,7 @@ function style_me(){
     document.getElementById("upcomingevents3").className = "upcomingevents";
 
 }
+
+function copyText() {
+    document.getElementById("scientistoutput").value = document.getElementById("scientistinput").value;
+}
